b53a6e609d3055b0f53fcdb2310e6e6446d988c5
Ethan Nelson <legobrickzmariomaster@gmail.com>
2025-03-29 19:58:48 -0700

    Readded images, css, and added html as ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Getting index!!");
   res.render('index', { title: 'Express' });
+});
+
+router.get('*.ejs', function(req, res, next) {
+  console.log("getting ejs");
+  // splitPath = ;
+  // filename = req.path.split("/")[-1];
+  // console.log(splitPath);
+  console.log(req.path.split("/").at(-1));
+  res.render(req.path.split("/").at(-1));
 });
 
 module.exports = router;
